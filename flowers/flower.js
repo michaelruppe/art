c75c80c4acbc8f341ea53626222a1a70719c6060
Michael Ruppe <michael.ruppe@live.com.au>
2017-05-03 21:55:51 +1000

    Draw flowers more sensibly

--- flowers/flower.js
+++ flowers/flower.js
@@ -1,21 +1,32 @@
 // ToDo: fix flower proportions
 
 function Flower() {
-	this.pos = createVector(random(width), height);
 	this.length = random(40,120);
+	this.pos = createVector(random(width), height-this.length);
 
+	this.size = 5;
+	this.theta = random(0,PI/2);
 
 	this.show = function(){
+		push();
+		translate(this.pos.x, this.pos.y-length);
+		rotate(this.theta);
+		// Petals
 	    stroke(155, 129, 186);
-	    strokeWeight(3);
-	    point(this.pos.x +1,this.pos.y-this.length);
-	    point(this.pos.x -1,this.pos.y-this.length);
-	    point(this.pos.x,this.pos.y-this.length +1);
-	    point(this.pos.x,this.pos.y-this.length -1);
+	    strokeWeight(this.size);
+	   	point(0, this.size);
+	   	point(this.size, 0);
+	   	point(0, -this.size);
+	   	point(-this.size, 0);
+	    ellipse(0,0, this.size, this.size);
 
-	    strokeWeight(1);
+	    // Pollen
+	    push();
 	    stroke(239,192,0)
-	    point(this.pos.x,this.pos.y-this.length);
+	    point(0,0);
+
+	    pop();
+	    pop();
 	    
 	}
 
--- flowers/sketch.js
+++ flowers/sketch.js
@@ -3,14 +3,14 @@ var flowers = []
 function setup() {
 	createCanvas(800,400);
 	
-	for (var i = 0; i < 100; i++ ) {
+	for (var i = 0; i < 200; i++ ) {
 		flowers[i] = new Flower();
 	}
 
 }
 
 function draw() {
-	background(120);
+	background(126,192,238); // sky blue
 	
 	for (var j = 0; j < flowers.length; j++ ) {
 		flowers[j].show();
