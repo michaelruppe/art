67d51dd6dd8f142b635dbcb7e4b7c0fec3e14ad1
michaelruppe <michael.ruppe@live.com.au>
2017-11-05 11:11:38 +1100

    Definitely works now. Closed loop by changing which bit encodes data.

--- steganograph/sketch.js
+++ steganograph/sketch.js
@@ -9,7 +9,7 @@ let buffer = [];
 let dBuffer=  [];
 let outputText = "";
 let txt, len;
-
+let encoderBit = 7; // Which bit in the pixel are we going to hide our data?
 
 
 
@@ -39,25 +39,24 @@ function setup() {
   }
 
   // Encode the source text into the destination image
-  // If the source text would use too many pixels, only loop through number of piels available.
+  // If the source text would use too many pixels, only loop through number of pixels available.
   // Show the image with text encoded
   image(desImg, srcImg.width,0);
   desImg.loadPixels();
   pixelCount = min( desImg.pixels.length/8, txt.length);
 
   for ( let i = 0; i < pixelCount; i++){
-    let intDecode = 0;
     for ( let j = 0; j < 8; j++){
         let currentPixel = intToBin( desImg.pixels[8*i + j] );
         // console.log("before "+currentPixel);
-        currentPixel[7] = buffer[i][j]; // Load buffer value into LSB of current pixel
+        currentPixel[encoderBit] = buffer[i][j]; // Load buffer value into LSB of current pixel
         // console.log("after "+currentPixel);
 
         // ARRAY to INT
         // Take 8-bits and create an integer
+        let intDecode = 0;
         for (let k = 0; k < 8; k++){
-          intDecode = 0;
-          if (currentPixel[k] == 1) intDecode += pow(2,7-k); // MSB. if it were LSB it would be pow(2,j)
+          if (currentPixel[k] == 1) intDecode += pow(2,7-k); // MSBfirst. if it were LSB it would be pow(2,j)
         }
         desImg.pixels[8*i+j] = intDecode;
         // desImg.pixels[8*i+j] = currentPixel;
@@ -72,7 +71,7 @@ function setup() {
     // Take 8-bits and create an integer
     for (let j = 0; j < 8; j++){
       let currentPixel = intToBin( desImg.pixels[8*i + j] );
-      if (currentPixel[7] == 1) intDecode += pow(2,7-j); // MSB. if it were LSB it would be pow(2,j)
+      if (currentPixel[encoderBit] == 1) intDecode += pow(2,7-j); // MSB first. if it were LSB it would be pow(2,j)
     }
 
     // Convert int to character
@@ -82,10 +81,15 @@ function setup() {
   createP(preTxt.toString());
   createP(outputText);
 
+
+
+
 }
 
 function draw() {
+  // background(127);
   image(desImg, srcImg.width,0); // make sure we're showing the destination image so there's no doubt...
+  desImg.updatePixels();
 }
 
 
