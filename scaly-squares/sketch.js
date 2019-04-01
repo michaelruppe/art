2aa591c7472043965b213c2ceb082562da8cd1c9
michaelruppe <michael.ruppe@live.com.au>
2019-04-02 07:41:34 +1100

    update scaly-squares

--- scaly-squares/sketch.js
+++ scaly-squares/sketch.js
@@ -1,36 +1,29 @@
 // I started by trying to recreate the famous work "Schotter" by Georg Nees
 // but it turned into a nice tiled flow field instead.
 
+
 let squares = [];
-let size = 30;
+let size = 30;  // Square size
 let cols, rows;
 let zoff = 0;
-let rubiks = ['white', 'red', 'blue', 'green', 'yellow', 'orange'];
+
 
 function setup() {
-  // colorMode(HSB,100);
-  let canvas = createCanvas(windowWidth,windowHeight);
-  // let canvas = createCanvas(800,800);
-
-  cols = floor(windowWidth/size)+2;
-  rows = floor(windowHeight/size)+2;
-
+  let canvas = createCanvas(800,800);
   canvas.parent('sketch-holder');
 
+  cols = floor(windowWidth/size)+2; // add some overflow so there are no gaps at canvas edges
+  rows = floor(windowHeight/size)+2;
 
   for (let i = 0; i < rows; i++) {
     squares[i] = [];
     for (let j = 0; j < cols; j++) {
-      let angle = noise(0.1*i,j*0.1, zoff) * TWO_PI;
+      let angle = noise(0.1 * i, 0.1 * j, zoff) * TWO_PI;
       squares[i].push(new Square(size*j, size*i, angle, size));
       squares[i][j].col = color(random(rubiks));
 
-
     }
   }
-
-
-
 
 }
 
@@ -38,25 +31,18 @@ function draw() {
   background(255);
   zoff += 0.01;
 
-  let from = color(255, 0, 0, 0.2 * 255);
-  let to = color(0, 0, 255, 0.2 * 255);
+  let from = color(255, 0, 0, 0.3 * 255);
+  let to = color(0, 0, 255, 0.3 * 255);
 
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-        let angle = noise(0.1*i,j*0.1, zoff) * TWO_PI;
+        let noiseVal = noise(0.1*i,j*0.1, zoff);
+        let angle =  noiseVal * TWO_PI;
         squares[i][j].ang = angle;
-        let temp = map(angle,-TWO_PI/8,TWO_PI/8,0,1);
-        let c = lerpColor(from, to, temp);
+
+        let c = lerpColor(from, to, noiseVal);
         squares[i][j].col = c;
-        // squares[i][j].col = color(255, 255, map(angle,0,TWO_PI,0,255));
-        // let colour = map(angle,-PI/4,PI/4,0,360);
-        // let colourString = 'hsl(' + nf(colour,NULL,0) + ',100%, 0%)';
-        // let colourString = 'hsl(120,100%, 50%)';
-
-        // squares[i][j].col = color(colourString);
-        // squares[i][j].col = color(colour,100,100);
         squares[i][j].show();
-
     }
   }
 }
@@ -67,9 +53,7 @@ class Square {
     this.y = y;
     this.ang = ang;
     this.dim = dim;
-    this.col;// = color('hsl(120,100%, 50%)');
-
-
+    this.col;
   }
 
   show() {
@@ -81,6 +65,4 @@ class Square {
     rect(0,0,this.dim,this.dim);
     pop();
   }
-
-
 }
