d21fd84767c145e1f851cf80fc06c3d85b24c81c
michaelruppe <michael.ruppe@live.com.au>
2019-04-22 12:08:18 +1000

    add fourier-trace basic functionality

---
+++ fourier-trace/fourier.js
@@ -0,0 +1,36 @@
+function dft(x) {
+  // Useful links:
+  // https://www.algorithm-archive.org/contents/cooley_tukey/cooley_tukey.html
+  // TODO: find the FFT formula on wiki so following notation fits
+  // could also do this operation with a PVector
+
+
+
+  const N = x.length;
+  let X = [];
+
+  for (let k = 0; k < N; k++) {
+    let re = 0;
+    let im = 0;
+
+    for (let n = 0; n < N; n++) {
+      let phi = TWO_PI * k * n / N; // simplify the brackets to some variable, Phi
+      re += x[n] * cos(phi);
+      im -= x[n] * sin(phi);
+
+    }
+    // average the contributions
+    re = re/N;
+    im = im/N;
+
+    // collect the output parameters
+    let freq = k;
+    let amp = sqrt(re*re + im*im);
+    let phase = atan2(im, re);
+
+    X[k] = { re, im, freq, amp, phase}; // making an object with 'enhanced object literals ES6'
+  }
+
+
+  return X;
+}
---
+++ fourier-trace/sketch.js
@@ -0,0 +1,70 @@
+let x = [];
+let y = [];
+let fourierX;
+let fourierY;
+let time = 0;
+let path = [];
+
+function setup() {
+  createCanvas(600,400);
+  for (let i = 0; i < 300; i++) {
+    angle = map(i,0,500,0,TWO_PI)
+    x[i] = 150*noise(angle*i/50);
+    y[i] = 150*noise(angle*i/50 + 1000);
+  }
+
+  fourierX = dft(x);
+  fourierY = dft(y);
+}
+
+function epicycles(x,y,rotation, fourier) {
+  for (let i = 0; i < fourier.length; i++) {
+    let prevx = x;
+    let prevy = y;
+
+    let freq = fourier[i].freq;
+    let radius = fourier[i].amp;
+    let phase = fourier[i].phase;
+    x += radius * cos(freq * time + phase + rotation);
+    y += radius * sin(freq * time + phase + rotation);
+
+    stroke(255,100); noFill();
+    ellipse(prevx, prevy, radius * 2);
+    stroke(255);
+    line(prevx, prevy, x, y);
+  }
+
+  return createVector(x,y);
+}
+
+
+function draw() {
+  background(0);
+
+  let vx = epicycles(300,80, 0, fourierX);
+  let vy = epicycles(70, 0.6*height, HALF_PI, fourierY);
+  let v = createVector(vx.x, vy.y);
+  path.unshift(v);
+
+
+  stroke(255,100);
+  line(vx.x,vx.y,v.x,v.y);
+  line(vy.x,vy.y,v.x,v.y);
+
+  beginShape(); stroke(255); noFill();
+  for (let i = 0; i < path.length; i++){
+    vertex(path[i].x,path[i].y);
+  }
+  endShape();
+
+
+
+
+  const dt = TWO_PI / fourierY.length;
+  time += dt;
+
+  if (path.length > 250) {
+    path.pop();
+  }
+
+}
