9a351ac8ff8c4eb396edf12660ac9855cec9bc96
Michael Ruppe <michael.ruppe@live.com.au>
2020-02-29 21:08:31 +1100

    Animated fireflies

--- firefly/sketch.js
+++ firefly/sketch.js
@@ -1,9 +1,11 @@
 /*******************************************************************************
- * Title - Description
+ * Firefly -  Flickering fireflies will attempt to synchronise with their
+ *            neightbours.
  * Michael Ruppe
  * April 2019
  *
- * Long description
+ * The fireflies flicker for now, but I haven't implemented the synchronisation
+ * behaviour - fireflies should be influenced by their neightbours' behaviour.
  ******************************************************************************/
 
 let fireflys = [];
@@ -13,9 +15,21 @@ function setup() {
   createCanvas(600,400);
   frameRate(30);
   for(let i=0; i<(width*height/5000); i++){
-    fireflys[i] = new Firefly();
+    fireflys[i] = new Firefly(random(width),random(height));
   }
 
+  //once all fireflys generated, populate each with a list of neighbour IDs
+  for(let i=0; i<fireflys.length; i++){
+    for(let j=0; j<fireflys.length; j++){
+      if(i == j) continue; //no need to compare against self
+      // ToDo Radius
+      if (abs(fireflys[i].x - fireflys[j].x) < 100 && abs(fireflys[i].y - fireflys[j].y) < 100){
+        // close neighbours
+        fireflys[i].neighbours.push(fireflys[j]);
+      }
+    }
+  }
+  // fireflys[0].wait = fireflys[1].wait;
 }
 
 function draw() {
@@ -23,28 +37,54 @@ function draw() {
   for(let i=0; i<fireflys.length; i++){
     fireflys[i].update();
     fireflys[i].show();
+
+    // TODO
+    //Evolve the timing to better suit neighbours
+    //check if neighbours states are different.
+    //if different, nudge start time/end time
+
   }
 
   frameCount++;
 }
 
-function Firefly() {
-  this.x = random(width);
-  this.y = random(height);
-  this.wait = floor(random(75,300));
+function Firefly(x,y) {
+  this.x = x;
+  this.y = y;
+  this.start = floor(random(-100,100));
+  if (this.start <= 0){
+    this.state = 'on';
+  }else{
+    this.state = 'off'
+  }
   this.duration = floor(random(20,100));
-  this.state = random(['on', 'off']);
-  this.state == 'on' ? this.start = -floor(random(this.wait)) : this.start //initialise start variable if initial state is 'on'. Don't initialise to 0 because the start looks unnatural
+  this.end = this.start + this.duration;
+
+  this.neighbours = [];
 
   this.update = function(){
-    //determine on/off state
-    if (frameCount % this.wait == 0 && this.state == 'off') {
+    //Determine when to blink on/off given current timing parameters
+    if (frameCount > this.start && this.state == 'off') {
       this.state = 'on';
-      this.start = frameCount;
+      this.end = frameCount + this.duration;
     }
-    else if (frameCount - this.start >= this.duration){
+    else if (frameCount > this.end && this.state == 'on'){
       this.state = 'off';
+      this.start = frameCount + this.duration;;
     }
+
+    // at transition to on, check state of neighbours and nudge time
+    for(let i in this.neighbours) {
+      if(this.state == 'off' && i.state == 'on'){
+
+      }
+      // check if neighbour is different state
+      if (this.start > i.start) this.start -= 5;
+      else if (this.start < i.start) this.start += 5;
+    }
+
+
+
   }
 
   this.show = function(){
