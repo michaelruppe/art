1e60321d2d3dcc1e492fdd97a9d0d30469fafeba
Michael Ruppe <michael.ruppe@live.com.au>
2020-03-04 22:17:35 +1100

    unique seed, update res for small screens, beautify

--- perf_animated/perf-p5/sketch.js
+++ perf_animated/perf-p5/sketch.js
@@ -11,26 +11,35 @@
  *
  ******************************************************************************/
 
-let increment = 0.1;
-let noise;
+let increment = 0.1; // Spatial noise increment
 let alpha = 255;
 
-  let xoff = 0.0; // Start xoff at 0
-  let yoff = 0.0;
-  let zoff = 0.0;
-  let resOriginal = 15;
-  let res = resOriginal;
+// Noise offsets
+let xoff = 0.0;
+let yoff = 0.0;
+let zoff = 0.0;
+let noise;
+
+// Resolution: nominal dot size
+let res = 15;
 let rows, cols;
 
 
 function setup() {
-  let canvas = createCanvas(windowWidth+10,windowHeight+10);
+  let canvas = createCanvas(windowWidth + 10, windowHeight + 10);
   canvas.parent("sketch-holder");
-  if(windowWidth * windowHeight < 600*600) res = resOriginal/2.5; // Increase density for small screens
-  let rows = height/res;
-  let cols = width/res;
+  if (windowWidth * windowHeight < 600 * 600) res = res / 2.5; // Increase density for small screens
+  let rows = height / res;
+  let cols = width / res;
 
-  simplex = new openSimplexNoise(3600*hour()+60*minute()*+second()); // Initialise with a (almost) unique seed
+  // Initialise with a seed unique to this very second
+  simplex = new openSimplexNoise(
+    3600*24*30*12 * year() +
+    3600*24*30 * month() +
+    3600*24 * day() +
+    3600 * hour() +
+    60 * minute() +
+    second());
 
 }
 
@@ -38,59 +47,56 @@ function draw() {
   background(0);
 
   fill(255); noStroke();
-  xoff=0;
-  yoff=0;
+  xoff = 0;
+  yoff = 0;
   zoff += 0.002;
-  for (let i = -1.5*res/2; i <= width; i += res) {
+  for (let i = -1.5 * res / 2; i <= width; i += res) {
     xoff += increment;
     yoff = 0;
-    for (let j = -1.5*res/2; j <= height; j += res) {
+    for (let j = -1.5 * res / 2; j <= height; j += res) {
       yoff += increment;
+
+      // This is where the effect is built. Only reneder some dots (chance
+      // based off noise) and set dot size as function of simplex noise
       let chance = simplex.noise3D(xoff, yoff, zoff);
-
-
       if (chance > 0.3) {
+        // Islands
+        let r = res * ( chance ) * 1.3;
+        ellipse(i, j, r, r);
+
         // Shimmery pattern
         // let r = random(res/3, res) * simplex.noise3D(xoff,yoff,zoff);
 
-        // Islands
-        let r = res* (simplex.noise3D(xoff,yoff,zoff))*1.3;
-        ellipse(i, j, r, r);
       }
-
-
     }
   }
-
-  displayHelperText(alpha-=1);
+  displayHelperText(alpha -= 1); // Displays the text at beginning of animation
 }
 
 function displayHelperText(_alpha) {
   let a = _alpha;
-  fill(0, a);
-  rectMode(RADIUS);
-  rect(width/2,height/2, 600,300);
-  fill(255,a);
-  let s = 'Click to toggle audio';
-  textAlign(CENTER, CENTER);
-  textSize(60);
-  text(s, width/2, height/2);
-
+  if (a > 0) {
+    fill(0, a);
+    rectMode(RADIUS);
+    rect(width / 2, height / 2, 600, 300);
+    fill(255, a);
+    let s = 'Click to toggle audio';
+    textAlign(CENTER, CENTER);
+    textSize(60);
+    text(s, width / 2, height / 2);
+  }
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth+10, windowHeight+10);
-  // recalculate density for small screens
-  if(windowWidth * windowHeight < 600*600) res = resOriginal/2.5;
-  else {res = resOriginal;}
-
-  rows = height/res;
-  cols = width/res;
+  resizeCanvas(windowWidth + 10, windowHeight + 10);
+  rows = height / res;
+  cols = width / res;
 }
 
 
 // Toggle the audio on click
 document.addEventListener('click', musicPlay);
+
 function musicPlay() {
   let audio = document.getElementById('player');
   if (audio.currentTime == 0) {
